2b134967fedf83a58f0e8938a9ce1dd65319914e
Rui <33549232+user-cube@users.noreply.github.com>
2026-01-24 15:30:35 +0000

    fix: r2 module - missing options (#6)
    * fix: add missing jurisdiction and storage_class
    
    * fix: update docs
    
    * docs: update terraform-docs for r2
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- modules/r2/main.tf
+++ modules/r2/main.tf
@@ -4,6 +4,8 @@ resource "cloudflare_r2_bucket" "buckets" {
   account_id = var.account_id
   name       = each.value.name
   location   = each.value.location
+  jurisdiction = each.value.jurisdiction
+  storage_class = each.value.storage_class
 
   lifecycle {
     create_before_destroy = true
