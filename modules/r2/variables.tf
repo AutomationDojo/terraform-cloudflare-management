f7579c3cfb44a14a953294a4d870b977df372ea6
Rui Coelho <ruicoelho@ua.pt>
2026-01-24 15:26:58 +0000

    fix: add missing jurisdiction and storage_class

--- modules/r2/variables.tf
+++ modules/r2/variables.tf
@@ -7,7 +7,9 @@ variable "buckets" {
   description = "List of R2 buckets to create"
   type = list(object({
     name     = string
-    location = optional(string, "EEUR")
+    location = optional(string, "eeur")
+    jurisdiction = optional(string, "eu")
+    storage_class = optional(string, "default")
   }))
   default = []
 }
