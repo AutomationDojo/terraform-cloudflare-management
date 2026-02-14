c6e1a715e51f0e426e27194d297df37d545e9fcb
Rui Coelho <ruicoelho@ua.pt>
2026-02-14 16:30:36 +0000

    feat: add cors and bucket domain support

--- modules/r2/variables.tf
+++ modules/r2/variables.tf
@@ -6,10 +6,24 @@ variable "account_id" {
 variable "buckets" {
   description = "List of R2 buckets to create"
   type = list(object({
-    name     = string
-    location = optional(string, "eeur")
-    jurisdiction = optional(string, "eu")
+    name          = string
+    location      = optional(string, "eeur")
+    jurisdiction  = optional(string, "eu")
     storage_class = optional(string, "Standard")
+    cors_rules = optional(list(object({
+      id              = optional(string)
+      allowed_methods = list(string)
+      allowed_origins = list(string)
+      allowed_headers = optional(list(string))
+      expose_headers  = optional(list(string))
+      max_age_seconds = optional(number)
+    })), [])
+    custom_domains = optional(list(object({
+      domain  = string
+      zone_id = string
+      enabled = optional(bool, true)
+      min_tls = optional(string)
+    })), [])
   }))
   default = []
 }
